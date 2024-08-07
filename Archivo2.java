f2514380001c9b5f6a46a1eeb4bdedc7b68c0b63
fran <franciscopieri123@gmail.com>
2024-08-06 21:01:35 -0300

    Mostrando status corto

---
+++ Archivo2.java
@@ -0,0 +1,16 @@
+public class Archivo2 {
+    public int paginas;
+    public String titulo;
+    public String subTitulo;
+    public Archivo2(int paginas, String titulo, String subtitulo){
+        this.paginas = paginas;
+        this.titulo = titulo;
+        this.subTitulo = subtitulo;
+    }
+    public String getTitulo(){
+        return titulo;
+    }
+    public void mensaje(){
+        System.out.println("Hola");
+    }
+}
